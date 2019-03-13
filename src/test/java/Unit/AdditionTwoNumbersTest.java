f6e2770c10c0804c290b0e5ed31f59df52fa1619
chmel <ext-viktor.chmel@here.com>
2019-03-13 19:55:44 +0200

    Add data providers

--- src/test/java/Unit/AdditionTwoNumbersTest.java
+++ src/test/java/Unit/AdditionTwoNumbersTest.java
@@ -2,32 +2,20 @@ package Unit;
 
 import com.mainacad.AdditionTwoNumbers;
 import org.junit.Assert;
-import org.junit.Test;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
 import static org.junit.Assert.*;
 
 public class AdditionTwoNumbersTest {
 
-    //TestdID|  a | b | expected
-    // 0 | 0 | 0
-    // 2 | 2 | 4
-
-    @DataProvider(name = "forAddingTest")
+    @DataProvider(name = "AddingTestDP")
     public Object[][] createData1() {
         return new Object[][] {
-                { 0, "Cedric", new Integer(36) },
-                { "Anne", new Integer(37)},
+                { 0, 0, 0 },
+                { 2, 2, 4},
         };
     }
-
-    //This test method declares that its data should be supplied by the Data Provider
-    //named "test1"
-    @Test(dataProvider = "test1")
-    public void verifyData1(String n1, Integer n2) {
-        System.out.println(n1 + " " + n2);
-    }
-
 
     private void add(int a,  int b, int expected) {
         AdditionTwoNumbers twoNumbers = new AdditionTwoNumbers( a, b);
@@ -35,8 +23,9 @@ public class AdditionTwoNumbersTest {
 
     }
 
-    @Test
-    public void runAddTest() {
-        add(0,0,0);
+    @Test(dataProvider = "AddingTestDP")
+    public void runAddTest( int a, int b, int expectedSum) {
+        System.out.println(a + " " + b + " " + expectedSum);
+        add(a, b, expectedSum);
     }
 }
