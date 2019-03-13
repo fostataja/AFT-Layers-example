c7fd59d1364b0ba4e9bd01f95f2bd79efb2e6ffd
chmel <ext-viktor.chmel@here.com>
2019-03-13 19:49:44 +0200

    log4j, testng, data providers

--- src/test/java/Frontend_UI/WebSiteTest.java
+++ src/test/java/Frontend_UI/WebSiteTest.java
@@ -8,10 +8,20 @@ import static com.codeborne.selenide.Condition.*;
 
 public class WebSiteTest {
 
+//    private static final Logger log = LogManager.getLogger("WebSiteTest");
+
    @Test
     public void userCanLoginByUsername() {
 
        open("https://novaposhta.ua/");
+
+//       log.trace("Trace Message!");
+//       log.debug("Debug Message!");
+//       log.info("Info Message!");
+//       log.warn("Warn Message!");
+//       log.error("Error Message!");
+//       log.fatal("Fatal Message!");
+
 
         $(By.name("q")).setValue("Тарифи").pressEnter();
 
--- src/test/java/Unit/AdditionTwoNumbersTest.java
+++ src/test/java/Unit/AdditionTwoNumbersTest.java
@@ -3,6 +3,7 @@ package Unit;
 import com.mainacad.AdditionTwoNumbers;
 import org.junit.Assert;
 import org.junit.Test;
+import org.testng.annotations.DataProvider;
 
 import static org.junit.Assert.*;
 
@@ -11,6 +12,22 @@ public class AdditionTwoNumbersTest {
     //TestdID|  a | b | expected
     // 0 | 0 | 0
     // 2 | 2 | 4
+
+    @DataProvider(name = "forAddingTest")
+    public Object[][] createData1() {
+        return new Object[][] {
+                { 0, "Cedric", new Integer(36) },
+                { "Anne", new Integer(37)},
+        };
+    }
+
+    //This test method declares that its data should be supplied by the Data Provider
+    //named "test1"
+    @Test(dataProvider = "test1")
+    public void verifyData1(String n1, Integer n2) {
+        System.out.println(n1 + " " + n2);
+    }
+
 
     private void add(int a,  int b, int expected) {
         AdditionTwoNumbers twoNumbers = new AdditionTwoNumbers( a, b);
