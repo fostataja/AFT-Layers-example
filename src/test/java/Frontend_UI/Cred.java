9ba78e0722acf41bdaabd64df0542f9dad28d21b
fostataja <fostataja@gmail.com>
2019-04-30 11:50:56 +0300

    add project

---
+++ src/test/java/Frontend_UI/Cred.java
@@ -0,0 +1,12 @@
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Cred {
+    public static String directoriesMenu = "i.fa.fa-archive";
+    public static String activities = "i.fa.fa-pencil";
+    public static String contracts = "div.left-menu-sub > div.parent-list > div > div > div";
+    public static String create = "div.quest-wrapp > div.paper-header > div.wrap > div.buttons> div";
+    public static String save = "button[type='submit']";
+
+}
